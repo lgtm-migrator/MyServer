2f4d3131602ac56f41a057c539f6346f801d9ff2
Marcon Willian <marconwillian@protonmail.com>
2020-06-14 19:18:34 -0300

    Change dependence of .env

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,8 +2,10 @@
 
 import express from 'express';
 import cors from 'cors';
-require('custom-env').env('dev');
+import dotenv from 'dotenv';
+
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
