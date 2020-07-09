b378386de96aa07214f188e03adb64678fbd9e8b
Marcon Willian <marconwillian@protonmail.com>
2020-07-09 07:33:05 -0300

    Fix ListController

--- packages/server/src/controllers/ListController.ts
+++ packages/server/src/controllers/ListController.ts
@@ -1,16 +1,32 @@
-import pm2 from 'pm2';
+import {Request, Response} from 'express';
+import pm2, {ProcessDescription} from 'pm2';
+
+interface Pm2Env {
+    node_verion: String;
+    versioning: String;
+    version: String;
+    node_version: String;
+    unstable_restarts: String;
+    restart_time: String;
+    status: String;
+}
 
 const ListController = {
-    index: (req, res) => {
-        pm2.list((err, processDescriptionList) => {
+    index: (req: Request, res: Response) => {
+        pm2.list((err, processDescriptionList: Array<ProcessDescription>) => {
             if (err) {
                 console.warn('PM2 list error', err);
                 return res.status(400).json(err);
             };
             const listProcess = processDescriptionList.map(process => {
                 const {pid, name} = process;
-                const {node_version, versioning, version, unstable_restarts, restart_time, status} = process.pm2_env;
-                return {pid, name, node_version, versioning, version, unstable_restarts, restart_time, status};
+                
+                if(process.pm2_env){
+                    const {node_version, versioning, version, unstable_restarts, restart_time, status} = (process.pm2_env as unknown) as Pm2Env;
+                    return {pid, name, node_version, versioning, version, unstable_restarts, restart_time, status};
+                } else {
+                    return {pid, name, status};
+                }
             })
 
             return res.status(200).json(listProcess);
