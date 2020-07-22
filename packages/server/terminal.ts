f51b39c7d73abe2f53a25de65e3162d2c59bb106
Marcon Willian <marconwillian@protonmail.com>
2020-07-21 23:39:23 -0300

    Update

--- packages/server/terminal.ts
+++ packages/server/terminal.ts
@@ -1,3 +1,28 @@
 const shell = require('shelljs');
 
 shell.exec('yarn add sms');
+
+
+/**
+ * 
+ * @todo List thinks that need finish todos
+ * @body  - [ ]  User
+ * @body    - [ ]  Create User
+ * @body    - [ ]  List Users
+ * @body    - [ ]  Auth Users
+ * @body    - [ ]  Get Data User
+ * @body - [ ]  List Services and Save id for name
+ * @body     - [ ]  Stop Service
+ * @body     - [ ]  Start Service
+ * @body     - [ ]  Restart Service
+ * @body     - [ ]  Update Deploy
+ * @body         - [ ]  Button
+ * @body         - [ ]  Generate Link webhook deploy
+ * @body             - [ ]  Config
+ * @body                 - Use tsc?
+ * @body                 - Execute pre code?
+ * @body                 - Execute migrates?
+ * @body                 - Execute yarn install
+ *  
+ * 
+ */
