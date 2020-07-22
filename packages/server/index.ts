2f759b38cb4da66054ff35d0b5f7436a875e8872
Marcon Willian <marconwillian@protonmail.com>
2020-07-21 23:38:04 -0300

    Update todo

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,27 @@
 import pm2 from 'pm2';
 import server from './src/server';
 
+/**
+ * @todo List thinks that need finish
+ * @body  - [ ]  User
+ * @body    - [ ]  Create User
+ * @body    - [ ]  List Users
+ * @body    - [ ]  Auth Users
+ * @body    - [ ]  Get Data User
+ * @body - [ ]  List Services and Save id for name
+ * @body     - [ ]  Stop Service
+ * @body     - [ ]  Start Service
+ * @body     - [ ]  Restart Service
+ * @body     - [ ]  Update Deploy
+ * @body         - [ ]  Button
+ * @body         - [ ]  Generate Link webhook deploy
+ * @body             - [ ]  Config
+ * @body                 - Use tsc?
+ * @body                 - Execute pre code?
+ * @body                 - Execute migrates?
+ * @body                 - Execute yarn install
+ *  
+ */
 
 pm2.connect(function(e) {
     if (e) return console.error('PM2 connection error', e);
