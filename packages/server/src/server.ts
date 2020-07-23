f07852418c0f2c5baade0f7c365ab8b57f3f3425
Marcon Willian <marconwillian@protonmail.com>
2020-07-23 15:00:53 -0300

    Update

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,8 +1,11 @@
 //https://github.com/vguillou/pm2-list-rest/blob/master/src/startHttpServer.js
+//https://github.com/rconjoe/pm2panel/blob/master/pm2panel.js
 
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+import getProcessPM2 from './services/getProcess';
 
 const app = express();
 dotenv.config();
@@ -15,7 +18,9 @@ routes(app)
 
 const port = process.env.PORT || 4404;
 
-export default () => { 
+export default async () => {
+    await getProcessPM2();
+    
     app.listen(port, () => {
         console.log(`HTTP server is listening on :${port}`);
     });
