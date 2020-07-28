7a996cb3a205a89582461a4bd1561773c20131b3
Marcon Willian <marconwillian@protonmail.com>
2020-07-27 22:33:14 -0300

    server - 0.2.2 | Config start list process

--- packages/server/src/controllers/ListController.ts
+++ packages/server/src/controllers/ListController.ts
@@ -1,36 +1,14 @@
 import {Request, Response} from 'express';
 import pm2, {ProcessDescription} from 'pm2';
 
-interface Pm2Env {
-    node_verion: String;
-    versioning: String;
-    version: String;
-    node_version: String;
-    unstable_restarts: String;
-    restart_time: String;
-    status: String;
-}
+import getProcess from '../services/getProcess.ts';
 
 const ListController = {
-    index: (req: Request, res: Response) => {
-        pm2.list((err, processDescriptionList: Array<ProcessDescription>) => {
-            if (err) {
-                console.warn('PM2 list error', err);
-                return res.status(400).json(err);
-            };
-            const listProcess = processDescriptionList.map(process => {
-                const {pid, name} = process;
-                
-                if(process.pm2_env){
-                    const {node_version, versioning, version, unstable_restarts, restart_time, status} = (process.pm2_env as unknown) as Pm2Env;
-                    return {pid, name, node_version, versioning, version, unstable_restarts, restart_time, status};
-                } else {
-                    return {pid, name, status};
-                }
-            })
+    index: async (req: Request, res: Response) => {
+        
+        let listProcess: Array<ProcessDescription> = await getProcess();
 
-            return res.status(200).json(listProcess);
-        });
+        return res.status(200).json(listProcess);
     }
 }
 
