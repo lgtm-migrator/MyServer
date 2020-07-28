3358e1ccf3424718c7027d64e90218ca8f9d704b
Marcon Willian <marconwillian@protonmail.com>
2020-07-27 22:33:14 -0300

    server - 0.2.2 | Config start list process

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import getProcessPM2 from './services/getProcess';
+import connection from './database/connection'/
 
 const app = express();
 dotenv.config();
@@ -19,8 +20,18 @@ routes(app)
 const port = process.env.PORT || 4404;
 
 export default async () => {
-    await getProcessPM2();
-    
+    let processList = await getProcessPM2();
+    await connection('processList')
+        .insert({
+		idProcess: processList.pid,
+		name: processList.name,
+		nodeVersion: processList.node_version,
+		versioning: processList.versioning,
+		version: processList.version,
+		unstableRestarts: processList.unstable_restarts,
+		status: processList.status,
+        })
+        
     app.listen(port, () => {
         console.log(`HTTP server is listening on :${port}`);
     });
