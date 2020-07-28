099779f242e41fab97e1052a67a73d49917ee61a
Marcon Willian <marconwillian@protonmail.com>
2020-07-27 22:32:37 -0300

    server 0.2.1 - Create Migrate of create process

--- packages/server/src/database/migrations/20200724215501_create_process_list.ts
+++ packages/server/src/database/migrations/20200724215501_create_process_list.ts
@@ -2,9 +2,19 @@ import * as Knex from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('processList', table => {
+		table.increments('id').primary();
+		table.integer('idProcess').notNullable();
+		table.string('name').notNullable();
+		table.string('nodeVersion');
+		table.string('versioning');
+		table.string('version');
+		table.string('unstableRestarts');
+        table.string('status');
+    });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('processList');
 }
 
