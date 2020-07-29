6bcb2c1cc2a6abc665ef054369d5a4eb65e04ba1
Marcon Willian <marconwillian@protonmail.com>
2020-07-28 22:31:28 -0300

    0.3.4 - Create controller for restart, stop and start

--- packages/server/src/Routes/ProcessRoute.ts
+++ packages/server/src/Routes/ProcessRoute.ts
@@ -5,8 +5,14 @@ import ProcessController from '../controllers/ProcessController';
 
 
 routes.get('/', ProcessController.index);
+
 routes.get('/monit/:id', ProcessController.monitItem);
 routes.get('/monit', ProcessController.monit);
+
+routes.get('/restart/:id', ProcessController.restart);
+routes.get('/start/:id', ProcessController.start);
+routes.get('/stop/:id', ProcessController.stop);
+
 
 routes.get('/:id', ProcessController.getItem);
 
--- packages/server/src/Routes/index.ts
+++ packages/server/src/Routes/index.ts
@@ -5,13 +5,13 @@ import ProcessController from '../controllers/ProcessController';
 import PullController from '../controllers/PullController';
 
 import UserRoute from './UserRoute';
-import UserProcess from './UserProcess';
+import ProcessRoute from './ProcessRoute';
 
 import middlewares from '../middlewares/auth';
 
 export default (app: Express) => {
     app.use('/user', UserRoute);
-    app.use('/process', UserProcess);
+    app.use('/process', ProcessRoute);
     app.use('/list', ProcessController.index);
     app.use('/pull', PullController.index);
 
