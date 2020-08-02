790a0fa7d4ded965250d276177fa4640c08809e8
Marcon Willian <marconwillian@protonmail.com>
2020-08-02 08:36:11 -0300

    server - 0.4.6 | Edit base of app

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,32 +1,24 @@
-//https://github.com/vguillou/pm2-list-rest/blob/master/src/startHttpServer.js
-//https://github.com/rconjoe/pm2panel/blob/master/pm2panel.js
+/** 
+ * @todo
+ * @body https://github.com/vguillou/pm2-list-rest/blob/master/src/startHttpServer.js https://github.com/rconjoe/pm2panel/blob/master/pm2panel.js
+ */
 
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-
+import { Server } from 'http';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const portOfApplication = process.env.PORT || 4404;
+
+import app from './app';
+app.set("port", portOfApplication);
+
+let server = new Server(app);
 
 import socker from './socker';
-
-const app = express();
-const port = process.env.PORT || 4404;
-app.set("port", port);
-
-let server = new http.Server(app);
 socker(server);
 
-app.use(cors());
-app.use(express.json());
-
-import routes from './Routes';
-routes(app)
-
-
 export default async () => {
-    server.listen(port, function() {
-      console.log(`listening on *:${port}`);
+    server.listen(portOfApplication, function() {
+      console.log(`listening on *:${portOfApplication}`);
     });
 }
