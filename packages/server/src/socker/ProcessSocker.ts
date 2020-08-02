cd52209dd46b9357f3d38ca2ed4b58cae736a5c3
Marcon Willian <marconwillian@protonmail.com>
2020-08-02 07:48:07 -0300

    server - 0.4.4 | Emit update for all sockers

--- packages/server/src/socker/ProcessSocker.ts
+++ packages/server/src/socker/ProcessSocker.ts
@@ -1,7 +1,7 @@
 import {Server} from "http";
 import ProcessController from '../controllers/ProcessController';
 import socketio from "socket.io";
-
+//https://medium.com/swlh/socket-io-games-the-right-way-using-nodejs-and-react-not-a-chat-app-part-1-e7a49d2f3f51
 const ProcessSocker = (server: Server) => {
     const io = socketio.listen(server, {
         path: '/socket/process'
@@ -31,16 +31,19 @@ const ProcessSocker = (server: Server) => {
         socket.on("process:start", async function(message: any) {
             console.log(message);
             socket.emit("process:start", await ProcessController.start(message));
+            socket.broadcast.emit("process:list", await ProcessController.index());
             socket.emit("process:list", await ProcessController.index());
         });
         socket.on("process:stop", async function(message: any) {
             console.log(message);
             socket.emit("process:stop", await ProcessController.stop(message));
+            socket.broadcast.emit("process:list", await ProcessController.index());
             socket.emit("process:list", await ProcessController.index());
         });
         socket.on("process:restart", async function(message: any) {
             console.log(message);
             socket.emit("process:restart", await ProcessController.restart(message));
+            socket.broadcast.emit("process:list", await ProcessController.index());
             socket.emit("process:list", await ProcessController.index());
         });
         socket.on("process:list", async function(message: any) {
