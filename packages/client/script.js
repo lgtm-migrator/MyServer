5bdc117d507115865dc508ec099ab4a9b08171ec
Marcon Willian <marconwillian@protonmail.com>
2020-08-02 16:57:02 -0300

    client - 0.0.4 | Add delete function

--- packages/client/script.js
+++ packages/client/script.js
@@ -22,7 +22,7 @@ const App = {
 
         for await (let process of processes){
             template += `<li><span>${process.pm_id} | ${process.name} - ${process.version} - ${process.pid} - ${uptime(process.pm_uptime)} - ${process.status==='online' ? '<span style="background: green">online</span>' : `<span style="background: red">${process.status}</span>`} - ${process.monit.cpu}% - ${Math.round((((process.monit.memory)/1012)/1012)*10)/10}mb 
-            - ${process.status==='online'? `<a href="#"  onclick="stop(${process.pm_id})">Stop<a>`: `<a href="#" onclick="start(${process.pm_id})">Start<a>`} - <a href="#" onclick="restart(${process.pm_id})">Restart<a></span>
+            - ${process.status==='online'? `<a href="#"  onclick="stop(${process.pm_id})">Stop<a>`: `<a href="#" onclick="start(${process.pm_id})">Start<a>`} - <a href="#" onclick="restart(${process.pm_id})">Restart<a> - <a href="#" onclick="deleteProcess(${process.pm_id})">Delete<a></span>
             `;
         }
         if(ulProcess){
