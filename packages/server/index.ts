de496268544a68903068a16dca8296d5cea28e79
Marcon Willian <marconwillian@protonmail.com>
2020-08-15 08:52:41 -0300

    Change name of server for listen

--- packages/server/index.ts
+++
@@ -1,7 +0,0 @@
-import pm2 from 'pm2';
-import server from './src/server';
-
-pm2.connect(function(e) {
-    if (e) return console.error('PM2 connection error', e);
-    server();
-});
--- packages/server/src/utils/generateNumberCode.ts
+++
@@ -1,8 +0,0 @@
-const generateNumberCode = (numberCaracteres: Number) => {
-	let count = 1;
-	let number = 1;
-	while (count < numberCaracteres) { count++; number = number*10; } 
-	return Math.floor(number + Math.random() * (number * 9));
-};
-
-export default generateNumberCode;
