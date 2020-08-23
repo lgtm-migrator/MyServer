52fb402f9b4c23ecff20f60dc085e11e12312c55
Marcon Willian <marconwillian@protonmail.com>
2020-08-23 19:18:15 -0300

    Add config of knex

---
+++ packages/server/knexfileConnection.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default  {
+    dev: {
+        client: 'sqlite3',
+        connection: {
+            filename: path.resolve(__dirname, 'src', 'database', 'dev.sqlite')
+        },
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+        },
+        useNullAsDefault: false
+    },
+    test: {
+        client: 'sqlite3',
+        connection: {
+            filename: path.resolve(__dirname, 'src', 'database', 'dev.sqlite')
+        },
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+        },
+        useNullAsDefault: false
+    },
+    production: {
+        migrations: {
+            directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+        },
+        client: 'mysql',
+        connection: {
+            host : process.env.DB_HOST,
+            user : process.env.DB_USER,
+            password : process.env.DB_PASSWORD,
+            database : process.env.DB_DATABASE
+        }
+    }
+};
