d2aa44892fda7c26b455b4b1b84169b0ce959a6d
Marcon Willian <marconwillian@protonmail.com>
2020-08-28 23:18:59 -0300

    chore: Updat Listen routes

--- packages/server/src/controller/ListenServerController.ts
+++ packages/server/src/controller/ListenServerController.ts
@@ -1,63 +1,63 @@
 import { Request, Response } from 'express';
 import connection from '../database/connection';
-import generateToken from '../utils/generateToken';
-
+import { generateTokenListen } from '../utils/generateToken';
 
 export default {
-    index: async (req: Request, res: Response) => {
-        let listServersListens = await connection('servers_listen')
-            .select('id', 'name', 'endpoint', 'status');
+  index: async (req: Request, res: Response): Promise<Response> => {
+    const listServersListens = await connection('servers_listen').select(
+      'id',
+      'name',
+      'endpoint',
+      'status'
+    );
 
-        return res.json(listServersListens);
-    },
-    create: async (req: Request, res: Response) => {
-        let { name, endpoint, token } = req.body;
+    return res.json(listServersListens);
+  },
+  create: async (req: Request, res: Response): Promise<Response> => {
+    const { name, endpoint, token } = req.body;
 
-        const [id] = await connection('servers_listen')
-            .insert({
-                name,
-                endpoint,
-                token,
-                status: true
-            });
+    const [id] = await connection('servers_listen').insert({
+      name,
+      endpoint,
+      token,
+      status: true
+    });
 
-        res.json({ id, name, endpoint, status: true });
-    },
-    edit: async (req: Request, res: Response) => {
-        let { id } = req.params;
-        let { name, endpoint, status } = req.body;
+    return res.json({ id, name, endpoint, status: true });
+  },
+  edit: async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    const { name, endpoint, status } = req.body;
 
-        await connection('servers_listen')
-            .where({ id })
-            .update({name, endpoint, status})
+    await connection('servers_listen')
+      .where({ id })
+      .update({ name, endpoint, status });
 
-        res.json({name, endpoint, status});
-    },
-    delete: async (req: Request, res: Response) => {
-        let { id } = req.params;
+    return res.json({ name, endpoint, status });
+  },
+  delete: async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
 
-        await connection('servers_listen')
-            .where({id})
-            .delete();
+    await connection('servers_listen').where({ id }).delete();
 
-        res.status(204).send();
-    },
-    auth: async (req: Request, res: Response) => {
-        let { id } = req.params;
+    return res.status(204).send();
+  },
+  auth: async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
 
-        let serverListen = await connection('servers_listen')
-            .where({id})
-            .first();
+    const serverListen = await connection('servers_listen')
+      .where({ id })
+      .first();
 
-        let params = {
-            type: 1
-        }
+    const params = {
+      type: 1
+    };
 
-        serverListen.token = generateToken(params, 3600, serverListen.token);
+    serverListen.token = generateTokenListen(params, 3600, serverListen.token);
 
-        res.json(serverListen);
-    },
-    monit: async (req: Request, res: Response) => {
-        res.json({message: 'Monit Server'})
-    }
-}
+    return res.json(serverListen);
+  },
+  monit: async (req: Request, res: Response): Promise<Response> => {
+    return res.json({ message: 'Monit Server' });
+  }
+};
