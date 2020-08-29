cc84bf2bebb2bb4be47d85918d81e7e7b515d381
Marcon Willian <marconwillian@protonmail.com>
2020-08-28 23:19:19 -0300

    Update Server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import app from './app';
 
-let port = process.env.PORT || 4003;
+const port = process.env.PORT || 4003;
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server listening on port ${port}`);
+});
